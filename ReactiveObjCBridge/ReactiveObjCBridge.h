272be3ad5d8a7b3fee5c038213f47110060215cb
Erich Grunewald <erichgrunewald@gmail.com>
2017-07-15 16:06:03 +0200

    Revert "Add RACScheduler+SwiftSupport.h to umbrella header."
    This reverts commit 9bb1904e5c8e87e5ff39f231a046ddf8c2eda2a8.

--- ReactiveObjCBridge/ReactiveObjCBridge.h
+++ ReactiveObjCBridge/ReactiveObjCBridge.h
@@ -8,8 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-#import "RACScheduler+SwiftSupport.h"
-
 //! Project version number for ReactiveObjCBridge.
 FOUNDATION_EXPORT double ReactiveObjCBridgeVersionNumber;
 
